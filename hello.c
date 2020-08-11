ff3ffd6c1637d6ef5a66eb268c1d1dbafc4cc7be
ala-valerie <ala-valerie@users.noreply.github.com>
2020-08-11 15:48:49 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <cs50.h>
+#include <cs50.h> //This allows you to use the data type string to create a string variable.
 
 int main(void)
 {
     string name = get_string("what is your name?");
     printf("Hello, %s\n", name);
 }
+
+// You use the relevant placeholder "%s" so that you can insert the name that will be stored in the name variable. 
+// The "\n" is used to go to the next line in the terminal. 
+// You need to finish some lines of code, like in line 6 and 7, with a semicolon.
