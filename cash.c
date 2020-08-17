a2871e84985f1940113cbdf2339db2b919c857bf
ala-valerie <ala-valerie@users.noreply.github.com>
2020-08-17 16:25:38 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -2,37 +2,40 @@
 #include <stdio.h>
 #include <math.h>
 
-int main (void)
+int main(void)
 { 
     float cash;
     do {
     cash = get_float("change owned: ");
-    }
+       }
     while (cash <= 0);
     
     int cents = round(cash*100);
     
-    
     int coins = 0;
+    
     while (cents >= 25)
-     { coins ++;
-       cents = cents -25;
-       
-     }
+    { 
+        coins ++;
+        cents = cents -25;
+    }
+     
     while (cents >= 10)
-    { coins ++;
-      cents = cents -10;
-      
+    { 
+        coins ++;
+        cents = cents -10;
     }
+     
     while (cents >= 5)
-    { coins ++;
-      cents = cents -5;
-      
+    {
+        coins ++;
+        cents = cents -5;
     }
+    
     while (cents >= 1)
-    { coins ++;
+    { 
+      coins ++;
       cents = cents - 1;
-      
     }
      
      printf("%i \n", coins);
