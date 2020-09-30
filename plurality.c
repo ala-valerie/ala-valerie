8e481503f890ac2a3944408e2d030800818c4b3d
ala-valerie <ala-valerie@users.noreply.github.com>
2020-09-30 11:37:46 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -65,14 +65,15 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-   for (int i = 0; i < candidate_count; i++) 
-  {
-     if (strcmp(candidates[i].name, name) == 0)
-     { 
-        candidates[i].votes ++;
-        return true;
-     }
+    for (int i = 0; i < candidate_count; i++) 
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        { 
+            candidates[i].votes ++;
+            return true;
+    }
   }
+            return false;
 }
 
 
@@ -81,21 +82,20 @@ void print_winner(void)
 {
   int highestvote = -1;
   for (int i = 0; i < candidate_count; i++)
-   { 
-     if (candidates[i].votes > highestvote)
+  { 
+    if (candidates[i].votes > highestvote)
+    {
+     highestvote = candidates[i].votes;
+    }
+  }
+  
+  for (i = 0; i < candidate_count; i++)
+  {
+     if (candidates[i].votes == highestvote)
      {
-        highestvote = candidates[i].votes;
+      printf("%s\n", candidates[i].name);
      }
-     
-   for (i = 0; i < candidate_count; i++)
-   {
-       if (candidates[i].votes == highestvote)
-       {
-           printf("%s\n", candidates[i].name);
-       }
-   }
-   } 
-   
+  }
   return;
 }
 
